a515c150b23c68014af12b03a13331c738f4c0c9
Alexei Mikhailov <amikhailov83@gmail.com>
2017-11-20 11:33:17 +0200

    Release v0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Salemove.HttpClient.Mixfile do
   def project do
     [
       app: :salemove_http_client,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env() == :prod,
