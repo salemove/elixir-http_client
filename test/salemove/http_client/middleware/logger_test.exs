97d60d129c5b0f7f932a0864b42ecce8db6a21f6
Alexei Mikhailov <amikhailov83@gmail.com>
2018-05-07 10:18:45 +0300

    Implement custom logger middleware
    Standard Tesla logger emits errors for all responses with code
    4xx and 5xx. While it's OK to have error messages for 5xx responses,
    some APIs may return 4xx code in normal situations. For this case
    configurable log level for certain response codes or code ranges
    is added.

---
+++ test/salemove/http_client/middleware/logger_test.exs
@@ -0,0 +1,80 @@
+defmodule Salemove.HttpClient.Middleware.LoggerTest do
+  use ExUnit.Case, async: false
+
+  defmodule Client do
+    use Tesla
+
+    plug(
+      Salemove.HttpClient.Middleware.Logger,
+      level: %{
+        422 => :info
+      }
+    )
+
+    adapter fn env ->
+      {status, body} =
+        case env.url do
+          "/connection-error" ->
+            raise %Tesla.Error{message: "adapter error: :econnrefused", reason: :econnrefused}
+
+          "/server-error" ->
+            {500, "error"}
+
+          "/client-error" ->
+            {404, "error"}
+
+          "/unprocessable-entity" ->
+            {422, "error"}
+
+          "/redirect" ->
+            {301, "moved"}
+
+          "/ok" ->
+            {200, "ok"}
+        end
+
+      %{env | status: status, headers: %{"content-type" => "text/plain"}, body: body}
+    end
+  end
+
+  import ExUnit.CaptureLog
+
+  setup do
+    Logger.configure(level: :info)
+  end
+
+  test "connection error" do
+    log =
+      capture_log(fn ->
+        assert_raise Tesla.Error, fn -> Client.get("/connection-error") end
+      end)
+
+    assert log =~ "/connection-error -> adapter error: :econnrefused"
+  end
+
+  test "server error" do
+    log = capture_log(fn -> Client.get("/server-error") end)
+    assert log =~ "/server-error -> 500"
+  end
+
+  test "client error" do
+    log = capture_log(fn -> Client.get("/client-error") end)
+    assert log =~ "/client-error -> 404"
+  end
+
+  test "client error with custom log level option" do
+    log = capture_log(fn -> Client.get("/unprocessable-entity") end)
+    assert log =~ "/unprocessable-entity -> 422"
+    assert log =~ "info"
+  end
+
+  test "redirect" do
+    log = capture_log(fn -> Client.get("/redirect") end)
+    assert log =~ "/redirect -> 301"
+  end
+
+  test "ok" do
+    log = capture_log(fn -> Client.get("/ok") end)
+    assert log =~ "/ok -> 200"
+  end
+end
