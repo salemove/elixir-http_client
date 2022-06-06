7760915305d7c4786fc2eb24eb038d31697586e3
Indrek Juhkam <indrek@urgas.eu>
2022-06-06 10:49:22 +0300

    Bump version to 2.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Salemove.HttpClient.Mixfile do
   def project do
     [
       app: :salemove_http_client,
-      version: "2.1.0-rc.2",
+      version: "2.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env() == :prod,
